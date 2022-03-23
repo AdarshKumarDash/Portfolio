ac888fdcf9c92bac39f5540ed76e4f3cc78a65c3
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-03-23 11:59:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const options = {
+    bottom: '100px',
+    right: '2px',
+    left: 'unset',
+    time: '0.3s',
+    mixColor: '#fff',
+    backgroundColor: '#fff',
+    buttonColorDark: '#100f2c',
+    buttonColorLight: '#fff',
+    saveInCookies: true,
+    label: '🌓',
+    autoMatchOsTheme: true
+}
+
+const darkmode = new Darkmode(options);
+
+darkmode.showWidget();
