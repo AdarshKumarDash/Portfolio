dc91d0c59a6e028aeaf31a1b46c01471cbf884ad
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-04-14 18:46:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,3 +23,10 @@ function myFunction() {
     preloader.style.display = "none";
     loader.style.display = "none";
 }
+
+let progress = document.getElementById("progressbar");
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function() {
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+}
